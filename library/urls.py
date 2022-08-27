41db799cc882d6f82be662222aa711e867782b91
olalekan temitayo <olalekantemitayo7@gmail.com>
2022-08-27 16:58:37 +0100

    implement add, update, view and delete user

--- library/urls.py
+++ library/urls.py
@@ -4,5 +4,7 @@ from library import views
 
 urlpatterns = [
     path('books/', views.BookList.as_view()),
-    path('books/<int:pk>/', views.BookDetail.as_view())
+    path('books/<int:pk>/', views.BookDetail.as_view()),
+    path('member/', views.UserList.as_view()),
+    path('member/<int:pk>', views.UserDetail.as_view())
 ]
--- library/views.py
+++ library/views.py
@@ -4,8 +4,9 @@ from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import BookSerializer
+from .serializers import BookSerializer, UserSerializer
 from .models import Book
+from authentication.models import User
 from rest_framework import serializers
 from drf_yasg.utils import swagger_auto_schema
 
@@ -69,4 +70,57 @@ class BookDetail(APIView):
         check_permission(request.user)
         book = self.get_object(pk)
         book.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class UserList(APIView):
+    """
+    List all books, or create a new book.
+    """
+
+    def get(self, request):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
+    @swagger_auto_schema(request_body=UserSerializer, operation_description="Login a user")
+    def post(self, request):
+        check_permission(request.user)
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserDetail(APIView):
+    """
+    Retrieve, update or delete a book instance.
+    """
+
+    def get_object(self, pk):
+        try:
+            return User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        user = self.get_object(pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    @swagger_auto_schema(operation_description="Login a user")
+    def patch(self, request, pk):
+        check_permission(request.user)
+        user = self.get_object(pk)
+        serializer = BookSerializer(user, data=request.data, context={"request": request}, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        check_permission(request.user)
+        user = self.get_object(pk)
+        user.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
