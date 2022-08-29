d399b71b2c5c827b33d0a65aea35a5bc03de803e
olalekan temitayo <olalekantemitayo7@gmail.com>
2022-08-29 01:16:01 +0100

    provised for postgres db

--- config/settings.py
+++ config/settings.py
@@ -85,7 +85,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES = {
-    
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 DATABASES['default'].update(db_from_env)
 DEBUG = False
